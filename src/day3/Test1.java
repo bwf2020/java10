e4fef06576894b54fcec3b1158951229c47cd0c2
bwf <bwf@bwf.com>
2021-04-06 15:46:24 +0800

    day3

---
+++ src/day3/Test1.java
@@ -0,0 +1,28 @@
+package day3;
+
+/**
+ *  关于Test的一个类
+ * @author 博为峰
+ */
+public class Test1 {
+
+	/**
+	 * 主函数main
+	 * @param args 数组参数
+	 */
+	public static void main(String[] args) {
+
+		int a = 100;// 定义一个变量
+		
+
+		/*
+		 * 给变量a 
+		 * 先用后加1 
+		 * 结果还是100 
+		 * 但是a的值应该是101了
+		 */
+		System.out.println(a++);
+
+	}
+
+}
---
+++ src/day3/Test2.java
@@ -0,0 +1,39 @@
+package day3;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println(777);
+		
+		System.out.println(0777);
+		
+		System.out.println(010);
+		
+		System.out.println(0x10);
+		
+		System.out.println(0b10);
+		
+		System.out.println(777L);
+		
+		System.out.println(3.14);
+		System.out.println(3.14F);
+		
+		System.out.println(3.14e6);
+		
+		System.out.println(0.3F+0.6F);
+		System.out.println(0.3+0.6);
+		
+		System.out.println(0.9F-0.6F);
+		
+		System.out.println(0.9-0.6);
+		
+		System.out.println(true);
+		
+		System.out.println(false);
+
+		
+	}
+
+}
