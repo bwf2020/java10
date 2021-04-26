b152ec73760b07551a2d0536646c69d31e836f9e
bwf <bwf@bwf.com>
2021-04-26 11:32:15 +0800

    day18

---
+++ src/day18/Test1.java
@@ -0,0 +1,45 @@
+package day18;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) throws Exception {
+
+
+		//静态实例化
+		User u1=new User();
+		
+		System.out.println(u1);
+		
+		//动态实例化
+		Class userClass=Class.forName("day18.User");
+		System.out.println(userClass);
+		User u2=(User) userClass.newInstance();
+		System.out.println(u2);
+		
+		//根据用户的输入实例化对象
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+		System.out.println("请输入要实例化的类的名字");
+		String className=sc.nextLine();
+		
+		/*if(className.equals("day18.User")) {
+			
+			User u3=new User();
+			System.out.println(u3);
+			
+		}else if(className.equals("day18.Dog")) {
+			
+			Dog dog=new Dog();
+			System.out.println(dog);
+		}*/
+
+		
+		Object obj=Class.forName(className).newInstance();
+		System.out.println(obj);
+		
+		}
+	}
+
+}
---
+++ src/day18/Test2.java
@@ -0,0 +1,148 @@
+package day18;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) throws Exception {
+
+		// 就是在DriverManager类中注册了一个驱动
+		Class.forName("com.mysql.jdbc.Driver");
+
+		// 可以从DriverManager获取数据库相关操作
+		// 数据库的连接
+		String url = "jdbc:mysql://localhost:3306/java10";
+		String user = "root";
+		String password = "123456";
+
+		Connection conn = DriverManager.getConnection(url, user, password);
+
+		System.out.println(conn);
+
+		// 创建sql命令，并且执行
+		Statement cmd = conn.createStatement();
+
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		 * System.out.println("请输入名字"); String name=sc.nextLine();
+		 * 
+		 * System.out.println("请输入性别"); String sex=sc.nextLine();
+		 * 
+		 * 
+		 * 
+		 * String sql=" insert into user(name,sex) values('"+name+"','"+sex+"') ";
+		 */
+		// 调用执行
+		/*
+		 * boolean flag= cmd.execute(sql);//重新编译
+		 * 
+		 * System.out.println(flag);
+		 */
+
+		/*
+		 * int rows=cmd.executeUpdate(sql); System.out.println(rows);
+		 */
+
+		/*
+		 * String sql=" update user set sex='w' "; int rows=cmd.executeUpdate(sql);
+		 * 
+		 * 
+		 * System.out.println(rows);
+		 */
+
+		/*
+		 * String sql=" delete from  user where id=2 "; int rows=cmd.executeUpdate(sql);
+		 * 
+		 * 
+		 * System.out.println(rows);
+		 */
+
+		// 通过预编译
+		/*String sql = " insert into user(name,sex) values(?,?) ";
+		PreparedStatement pcmd = conn.prepareStatement(sql);// 预编译
+
+		while (true) {
+
+			System.out.println("请输入名字");
+			String name = sc.nextLine();
+
+			System.out.println("请输入性别");
+			String sex = sc.nextLine();
+
+			pcmd.setString(1, name);
+			pcmd.setString(2, sex);
+
+			pcmd.executeUpdate();
+
+		}*/
+
+		
+		/*System.out.println("请输入编号");
+		String id=sc.nextLine();*/
+		
+		
+	/*	String sql=" delete from user where id="+id;
+		
+		cmd=conn.createStatement();
+		
+		cmd.executeUpdate(sql);*/
+		
+		
+		/*String sql = " delete from user where id=? ";
+		PreparedStatement pcmd = conn.prepareStatement(sql);
+		
+		pcmd.setString(1, id);
+		
+		pcmd.executeUpdate();*/
+		
+		String sql=" select * from user where 1=1 ";
+		PreparedStatement pcmd = conn.prepareStatement(sql);
+		
+		
+		
+		ResultSet rs= pcmd.executeQuery();
+		
+		/*System.out.println(rs.next());
+		
+		int id= rs.getInt("id");
+		System.out.println(id);
+		
+		String name=rs.getString("name");
+		System.out.println(name);
+		
+		System.out.println(rs.next());
+		
+		id= rs.getInt("id");
+		System.out.println(id);
+		
+		name=rs.getString("name");
+		System.out.println(name);
+		
+		
+		System.out.println(rs.next());*/
+	/*	id= rs.getInt("id");
+		System.out.println(id);*/
+		
+		
+		while(rs.next()) {
+			
+			int id= rs.getInt("id");
+			System.out.println(id);
+			
+			String name=rs.getString("name");
+			System.out.println(name);
+			
+		}
+		
+		System.out.println("没有数据了");
+		
+		
+	}
+
+}
